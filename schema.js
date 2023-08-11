da165dbb9ece27ffdacc28640a04797b0c01eefe
shahrukh khan <shahrukh.khan072@gmail.com>
2023-08-11 15:43:00 +0530

    add schema

--- schema.js
+++ schema.js
@@ -1,25 +1,42 @@
 export const typeDefs = `#graphql
-type Game {
-     id: ID!,
-     title: String,
-     platform: [String]
-
-}
-type Review {
-    id:ID!,
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
+  }
+  type Review {
+    id: ID!
     rating: Int!
     content: String!
-}
-type Author{
+    author: Author!
+    game: Game!
+  }
+  type Author {
     id: ID!
     name: String!
     verified: Boolean!
-}
-type Query {
-    reviews: [Review]
+    reviews: [Review!]
+  }
+  type Query {
     games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
     authors: [Author]
-
-}
-
-`;
+    author(id: ID!): Author
+  }
+  type Mutation {
+    addGame(game: AddGameInput!): Game
+    deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput): Game
+  }
+  input AddGameInput {
+    title: String!,
+    platform: [String!]!
+  }
+  input EditGameInput {
+    title: String,
+    platform: [String!]
+  }
+`
